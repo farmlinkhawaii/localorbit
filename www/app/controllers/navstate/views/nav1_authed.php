56145cb34004d0696e6f9141c42125cf24684001
lo-svn <lo-svn>
2012-12-06 16:20:18 +0000

    Modified nav includes for new UI
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@212 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/navstate/views/nav1_authed.php
+++ www/app/controllers/navstate/views/nav1_authed.php
@@ -1,7 +1,18 @@
-<?=$core->i18n['greeting']?> <?=$core->session['first_name']?>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://localorbit.zendesk.com/forums" target="_blank">help</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?=$core->config['app_page']?>#!auth-logout" onclick="core.go(this.href);"><?=$core->i18n['nav1:logout']?></a>
+<ul class="nav pull-right">
+	<li class="divider-vertical"></li>
+	<li><a href="<?=$core->config['app_page']?>#!dashboard-home" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:dashboard']?></a></li>
+	<li class="divider-vertical"></li>
+	<li><a href="https://localorbit.zendesk.com/forums">Help</a></li>
+	<li class="divider-vertical"></li>
+	<li><a href="#">Your Cart</a></li>
+	<li class="divider-vertical"></li>
+	<li><a href="<?=$core->config['app_page']?>#!auth-logout" onclick="core.go(this.href);"><?=$core->i18n['nav1:logout']?></a></li>
+</ul>
+<p class="navbar-text pull-right">
+	<?=$core->i18n['greeting']?> <a href="<?=$core->config['app_page']?>#!dashboard-home" onclick="core.go(this.href);" class="navbar-link"><?=$core->session['first_name']?></a>
+</p>
 <? core::replace('nav1top');?>
-	<a href="<?=$core->config['app_page']?>#!dashboard-home" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:dashboard']?></a>
-	<a href="<?=$core->config['app_page']?>#!market-info" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:marketinfo']?></a>
-	<a href="<?=$core->config['app_page']?>#!sellers-oursellers" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:oursellers']?></a>
-	<a href="<?=$core->config['app_page']?>#!catalog-shop" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:shop']?></a>
-<? core::replace('nav1sub');?>
+<li><a href="<?=$core->config['app_page']?>#!catalog-shop" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:shop']?></a></li>
+<li><a href="#">News</a></li>
+<li><a href="<?=$core->config['app_page']?>#!market-info" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:marketinfo']?></a></li>
+<? core::replace('mainnav');?>
--- www/app/controllers/navstate/views/nav1_unauthed.php
+++ www/app/controllers/navstate/views/nav1_unauthed.php
@@ -1,4 +1,9 @@
-<a onclick="$('#overlay,#popup3,#popup_closer').fadeIn(150);">contact</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a style="color: #555;" href="/login.php" onclick="core.go(this.href);"><?=$core->i18n['nav1:login']?></a>
+<ul class="nav pull-right">
+	<li class="divider-vertical"></li>
+	<li><a onclick="$('#overlay,#popup3,#popup_closer').fadeIn(150);">Contact</a></li>
+	<li class="divider-vertical"></li>
+	<li><a style="color: #555;" href="/login.php" onclick="core.go(this.href);"><?=$core->i18n['nav1:login']?></a></li>
+</ul>
 <? core::replace('nav1top');?>
-	&nbsp;&nbsp;
+&nbsp;
 <? core::replace('nav1sub');?>
