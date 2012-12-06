625ca459c3ac5b13718ed06e5bbaa907a51d52d4
lo-svn <lo-svn>
2012-12-06 14:38:47 +0000

    beginning redesign
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@209 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

---
+++ www/css/less.php
@@ -0,0 +1,64 @@
+<?
+$files = array(
+  'fonts','tags','structure','nav','forms','catalog','headings','datatable','popups','rte','misc','slideshow','public',
+);
+
+function is_color ($value) {
+  return preg_match('/^\s*#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\s*$/', $value);
+}
+
+function format_value ($value) {
+  return is_color($value) ? $value : "'" . $value . "'";
+}
+
+class core
+{
+  function __construct()
+  {
+    $this->config = array();
+  }
+}
+global $core;
+$core = new core();
+$path = dirname(__FILE__);
+include($path.'/../app/config.php');
+include(dirname(__FILE__) . '/../libraries/lessc.inc.php');# This is a list of all files we'll need to load
+
+# connect to the database to query for values
+
+mysql_connect($core->config['db']['hostname'],$core->config['db']['username'],$core->config['db']['password']);
+mysql_select_db($core->config['db']['database']);
+
+# the query should retrieve all default values, plus overrides on a per-domain basis
+$sql = '
+  select t.*,tor.override_value
+  from template_options t
+  left join template_option_overrides tor on (
+    t.tempopt_id=tor.tempopt_id and tor.domain_id in (
+      select domain_id
+      from domains
+      where hostname=\''.$_SERVER['HTTP_HOST'].'\'
+    )
+  );
+';
+
+# build a hash of all options
+$opts = mysql_query($sql);
+
+$options = array();
+while($opt = mysql_fetch_assoc($opts))
+{
+  # use the override if we find one
+  if($opt['override_value'] == 'NULL' || is_null($opt['override_value']))
+    $options[$opt['name']] = $opt['default_value'];
+  else
+    $options[$opt['name']] = $opt['override_value'];
+}
+//print_r($options);
+$less = new lessc;
+$less->setVariables(array_map('format_value', $options));//array('p1c' => '#333'));
+$less->setImportDir($path . '/../less');
+
+//header('Content-type: text/css');
+echo $less->compileFile($path . '/../less/bootstrap.less');
+?>
