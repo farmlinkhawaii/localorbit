257b619d4d44451c8ac2cae4a5058045e6ac93fd
lo-svn <lo-svn>
2012-12-10 03:03:28 +0000

    - Styling for catalog left nav - FILTERING NOW BROKEN
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@222 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/left_filters.php
+++ www/app/controllers/catalog/views/left_filters.php
@@ -4,33 +4,52 @@ global $core;
 $cats = $core->view[0];
 $sellers = $core->view[1];
 ?>
-<br />
+
+<!--
 <a href="#!catalog-shop" onmouseover="$('#catalog_note').fadeIn(300);" onmouseout="$('#catalog_note').fadeOut(300);"><img src="/img/misc/ellipsis_bubble.png" /></a><br />
 <div id="catalog_note"><div>To view a single category, click once, then click again to view all!</div></div>
-<div class="header_4 filter catalog_showall" onclick="core.catalog.resetFilters();">Show All</div>
-<div class="header_3 filter"><input type="checkbox" class="filtercheck" disabled="disabled" checked="checked" /> Filter by Sellers</div>
+-->
 
+<small style="position: relative; bottom: -1.6em;" class="pull-right hoverpointer" onclick="core.catalog.resetFilters();"><button class="close pull-left">&times;&nbsp;</button> Reset Filter</small>
+<h2>Catalog</h2>
+
+<hr style="margin: 5px 0;">
+
+<h3><!--<input type="checkbox" class="filtercheck" disabled="disabled" checked="checked" />--> Sellers</h3>
+
+<ul class="nav nav-list">
 <?php
 foreach($sellers as $seller)
 {
 	if($seller[0]['name'] != '')
 	{
 	?>
-	<div id="filter_org_<?=$seller[0]['org_id']?>" class="subheader_3 filter_subcat filter_org filter" onclick="core.catalog.setFilter('seller',<?=$seller[0]['org_id']?>);"><?=$seller[0]['name']?></div>
+	<li id="filter_org_<?=$seller[0]['org_id']?>"><a onclick="core.catalog.setFilter('seller',<?=$seller[0]['org_id']?>);"><?=$seller[0]['name']?></a></li>
 	<?
 	}
 }
-echo('<br />');
+?>
+</ul>
+
+<hr style="margin: 5px 0;">
+
+<h3>Categories</h3>
+<ul class="nav nav-list">
+<?
 $style=1;
 foreach($cats->roots as $root)
 {
 	$cat = $cats->by_id[$root]; 
 ?>
-<div onclick="core.catalog.setFilter('cat1',<?=$cat[0]['cat_id']?>);" class="filter filter_cat_<?=$cat[0]['cat_id']?> header_<?=$style?>">
-	<input type="checkbox" id="filtercheck_<?=$cat[0]['cat_id']?>" class="filtercheck" checked="checked" />
-	<?=$cat[0]['cat_name']?>
-</div>
-	<? 
+<li id="filter_cat_<?=$cat[0]['cat_id']?>">
+	<a onclick="core.catalog.setFilter('cat1',<?=$cat[0]['cat_id']?>);">
+		<!--<input type="hidden" id="filtercheck_<?=$cat[0]['cat_id']?>" class="filtercheck" checked="checked" />-->
+		<?=$cat[0]['cat_name']?>
+	</a>
+</li>
+
+
+	<? /* Subcategory
 	$subs = $cats->by_parent[$cat[0]['cat_id']];
 	if(isset($cats->by_parent[$cat[0]['cat_id']]))
 	{
@@ -44,7 +63,9 @@ foreach($cats->roots as $root)
 		}
 	}
 	echo('<br />');
+	*/
 	$style=($style == 1)?2:1;
 }
 ?>
+</ul>
 <? core::replace('left'); ?>
