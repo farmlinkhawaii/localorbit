9723699f8382f53417283729a4a71d083b1b97e4
lo-svn <lo-svn>
2012-12-13 17:26:00 +0000

    - Top nav edits
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@268 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/navstate/views/nav1_authed.php
+++ www/app/controllers/navstate/views/nav1_authed.php
@@ -10,14 +10,14 @@
 		</a>
 	</li>
 	<li class="divider-vertical"></li>
-	<li><a href="#!catalog-checkout"><i class="icon-shopping-cart icon-white" style="position: relative; top: 2px;"></i> Your Cart</a></li>
+	<li><a href="#!catalog-checkout"><i class="icon-shopping-cart icon-white"></i> Your Cart</a></li>
 	<li class="divider-vertical"></li>
 	<li><a href="https://localorbit.zendesk.com/forums">Help</a></li>
 	<li class="divider-vertical"></li>
 	<li><a href="<?=$core->config['app_page']?>#!auth-logout" onclick="core.go(this.href);"><?=$core->i18n['nav1:logout']?></a></li>
 </ul>
 <p class="navbar-text pull-right">
-	<?=$core->i18n['greeting']?> <a href="<?=$core->config['app_page']?>#!dashboard-home" onclick="core.go(this.href);" class="navbar-link"><?=$core->session['first_name']?></a>
+	<?=$core->i18n['greeting']?> <?=$core->session['first_name']?>
 </p>
 <? core::replace('nav1top');?>
 <li><a href="<?=$core->config['app_page']?>#!catalog-shop" onclick="core.go(this.href);" class="main"><?=$core->i18n['nav1:shop']?></a></li>
