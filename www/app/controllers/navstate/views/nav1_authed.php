f0e45ff3815efeb0e7183d373409099b0f856566
lo-svn <lo-svn>
2012-12-18 17:09:35 +0000

    working on your cart
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@321 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/navstate/views/nav1_authed.php
+++ www/app/controllers/navstate/views/nav1_authed.php
@@ -1,11 +1,11 @@
 <script type="text/javascript" charset="utf-8">
 $(function()
 	{
-		// Call stylesheet init so that all stylesheet changing functions 
+		// Call stylesheet init so that all stylesheet changing functions
 		// will work.
 		$.stylesheetInit();
-		
-		// This code loops through the stylesheets when you click the link with 
+
+		// This code loops through the stylesheets when you click the link with
 		// an ID of "toggler" below.
 		$('#toggler').bind(
 			'click',
@@ -15,7 +15,7 @@ $(function()
 				return false;
 			}
 		);
-		
+
 		// When one of the styleswitch links is clicked then switch the stylesheet to
 		// the one matching the value of that links rel attribute.
 		$('.styleswitch').bind(
@@ -50,7 +50,55 @@ $(function()
 		</a>
 	</li>
 	<li class="divider-vertical"></li>
-	<li><a href="#!catalog-checkout"><i class="icon-shopping-cart icon-white"></i> Your Cart</a></li>
+	<li class="dropdown">
+		<a class="dropdown-toggle" data-toggle="dropdown" href=""><i class="icon-shopping-cart icon-white"></i> Your Cart</a>
+		<div class="dropdown-menu span3 yourCart">
+			<?
+		$cart = core::model('lo_order')->get_cart();
+		$cart->load_items();
+		$item_hash = $cart->items->to_hash('prod_id');
+		foreach ($item_hash as $prod_id => $item) {
+			$prod = core::model('products')->load($item[0]['prod_id']);
+			//print_r($prod->__data);
+			?>
+				<div class="row">
+					<span class="span1 product-image">
+						<? if(intval($prod['pimg_id']) > 0){?>
+						<img class="img-polaroid catalog" src="/img/products/cache/<?=$prod['pimg_id']?>.<?=$prod['width']?>.<?=$prod['height']?>.100.75.<?=$prod['extension']?>" />
+						<?}else{?>
+						<img class="img-polaroid catalog_placeholder" src="<?=image('product_placeholder_small')?>" />
+						<?}?>
+					</span>
+					<span class="span2">
+						<div><?=$item[0]['product_name']?></div>
+						<div>Quantity: <?=$item[0]['qty_ordered']?> <?=(($item[0]['qty_ordered']>1)?$item[0]['unit_plural']:$item[0]['unit'])?></div>
+					</span>
+				</div>
+			<?
+/*
+rray ( [1844] => Array ( [0] => Array (
+lo_liid] => 6796 [lo_oid] => 3520 [lo_foid] => 0 [seller_mage_customer_id] => [seller_name] => DuRussel Farms [sku] => [product_name] => Baking Potatoes - #1 [qty_ordered] => 1
+[qty_adjusted] => 0 [qty_delivered] => 0 [unit] => Bag [unit_price] => 9.6000 [row_total] => 9.6000 [unit_plural] => Bags
+ [row_adjusted_total] => 9.6000 [adjusted_description] => [prod_id] => 1844 [addr_id] => [dd_id] => 34 [due_time] => 1356321600
+ [deliv_time] => [seller_org_id] => 1067 [lodeliv_id] => 2186 [last_status_date] => 1355775456 [lbps_id] => 1 [ldstat_id] => 1
+ [lsps_id] => 1 [category_ids] => 2,227,232,272,477 [final_cat_id] => 477 [producedat_address_id] => 1118 [producedat_org_id] => 1067
+ [producedat_address] => 4800 Esch Rd [producedat_city] => Manchester [producedat_region_id] => 33 [producedat_postal_code] => 48158
+ [producedat_telephone] => (734) 428-8900 [producedat_fax] => [producedat_delivery_instructions] => [producedat_longitude] => [producedat_latitude] =>
+ [has_valid_inventory] => 1 [has_valid_delivs] => 1 [has_valid_prices] => 1 ) ) )
+*/
+		}
+		?>
+			<div class="row">
+				<span class="span3">
+					<a class="btn btn-block" href="#!catalog-checkout">
+						<span class="pull-left">View Cart</span>
+						&nbsp;
+						<span class="pull-right">Subtotal</span>
+					</a>
+				</span>
+			</a>
+		</div>
+	</li>
 	<li class="divider-vertical"></li>
 	<li><a href="https://localorbit.zendesk.com/forums">Help</a></li>
 	<li class="divider-vertical"></li>
