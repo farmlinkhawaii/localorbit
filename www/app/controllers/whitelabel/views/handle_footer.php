0d8172aca0086957fc2d4e7584a2299bd0a715ed
lo-svn <lo-svn>
2012-12-18 22:04:43 +0000

    matching footer to wireframes
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@344 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/whitelabel/views/handle_footer.php
+++ www/app/controllers/whitelabel/views/handle_footer.php
@@ -19,57 +19,18 @@ $opts = core::model('template_options')->get_options(array('footer'));
 			</div>
 		</div>
 
-		<?/*
-		<div class="span4">
-		<? if ($core->config['domain']['domain_id'] > 1): ?>
-
-		<? else:
-			for ($i = 1; $i < 5; $i++) {
-	
-				if($opts['footer-col'.$i.'-label'] != ''){ ?>
-
-				
-				<img src="<?=$opts['footer-col'.$i.'-image']?>" /><br />
-				<b class="footer"><?=$opts['footer-col'.$i.'-label']?></b>
-				<ul class="footer">
-				<?for ($j = 1; $j < 12; $j++){?>
-					<?if($opts['footer-col'.$i.'-link'.$j.'-href'] !=''){?>
-					<li class="footer">
-						<? if($i == 4 && $j == 2){?>
-						<a class="footer" href="#" onclick="$('#overlay,#popup3,#popup_closer').fadeIn(150);">Send Us A Note Today!</a>
-						<?}else{?>
-						<a class="footer"<?
-					
-						# if a link is external to LO, open it in a new tab/window
-						if(strpos($opts['footer-col'.$i.'-link'.$j.'-href'],'http') === false)
-						{
-							if(strpos($opts['footer-col'.$i.'-link'.$j.'-href'],'#!') !== false)
-							{
-								echo(' onclick="core.go(this.href);"');
-							}
-						}
-						else
-						{
-							echo(' target="_blank"');
-						}
-					
-						?> href="<?=$opts['footer-col'.$i.'-link'.$j.'-href']?>">
-							<?=$opts['footer-col'.$i.'-link'.$j.'-label']?>
-						</a>
-						<?}?>
-					</li>
-					<?}?>
-				<?}?>
-				</ul>
-				<?}?>
-			<? }
-		endif;
-		?>
-		</div>
-		*/?>
+		
 
 		<div class="span6 tos">
-			<small><a href="app.php#!misc-tos" onclick="core.go(this.href);">Terms of Service</a> | <a href="app.php#!misc-localorbit_privacy" onclick="core.go(this.href);">Privacy</a> | <a href="http://localorbit.zendesk.com/forums" target="_blank">Help</a></small>
+			<?if(trim($core->config['domain']['secondary_contact_name']) != ''){?>
+				<b>Contact <?=$core->config['domain']['name']?></b><br />
+				<small><a href="mailTo:<?=$core->config['domain']['secondary_contact_email']?>"><?=$core->config['domain']['secondary_contact_name']?></a><br />
+				<?if(trim($core->config['domain']['secondary_contact_phone']) != ''){?>
+					T: <?=$core->config['domain']['secondary_contact_phone']?><br>
+				<?}?>
+				</small>
+				<br />
+			<?}?>
 		</div>
 	
 	</div>
