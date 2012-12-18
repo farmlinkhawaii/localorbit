f9de17253ec04f4079323e8ba0a926b47a2307e2
lo-svn <lo-svn>
2012-12-18 20:40:04 +0000

    - Close button for dashboard note
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@334 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/dashboard/views/dashboard_note.php
+++ www/app/controllers/dashboard/views/dashboard_note.php
@@ -1,5 +1,6 @@
 <?if($core->config['domain']['dashboard_note'] != ''){?>
 	<div class="alert">>
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
 		<?=$core->config['domain']['dashboard_note']?>
 	</div>
 <?}?>
