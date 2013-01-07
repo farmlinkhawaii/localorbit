6ced55d511611bd20f79f59cc76b798538af5feb
lo-svn <lo-svn>
2013-01-07 23:24:40 +0000

    making user list more consistent
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@407 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/models/customer_entity.php
+++ www/app/models/customer_entity.php
@@ -145,6 +145,7 @@ function enable_suspend_links($data)
 {
 	$data['activate_action'] = ($data['is_active'] == 1)?'deactivate':'activate';
 	$data['enable_action'] = ($data['is_enabled'] == 1)?'suspend':'enable';
+	$data['enable_icon'] = ($data['is_enabled'] == 1)?'minus':'plus';
 	return $data;
 }
 
