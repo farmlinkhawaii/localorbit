a8cc1ff1820456c997516c5c493f191a11d79948
lo-svn <lo-svn>
2013-01-11 13:32:54 +0000

    fixing up form fields
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@486 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/payment_purchaseorder.php
+++ www/app/controllers/catalog/views/payment_purchaseorder.php
@@ -5,19 +5,12 @@ if($org['payment_allow_purchaseorder'] == 1)
 {
 	$style = ($core->view[0] > 1)?' style="display:none;"':'';
 ?>
-<div id="payment_purchaseorder" class="payment_option form"<?=$style?>>
+<div id="payment_purchaseorder" class="payment_option span6 form"<?=$style?>>
 	<h4>Purchase Order Information</h4>
 	<br />
-	<table class="form">
-		<? if ($core->config['domain']['po_due_within_days'] > 0) { ?>
-		<tr>
-			<td colspan="2">Payment should be made in <?=$core->config['domain']['po_due_within_days']?> days.</td>
-		</tr>
-		<? } ?>
-		<tr>
-			<td class="label">Purchase Order #</td>
-			<td class="value"><input type="text" style="width: 150px;" name="po_number" value="" /></td>
-		</tr>
-	</table>
+	<?=core_form::input_text('Purchase Order #','po_number','')?>
+	<? if ($core->config['domain']['po_due_within_days'] > 0) { ?>
+	Payment should be made in <?=$core->config['domain']['po_due_within_days']?> days.
+	<? } ?>
 </div>
 <?}?>
