4a3b653abcc040557b06343e8b90f02862c2892c
lo-svn <lo-svn>
2013-01-14 20:51:30 +0000

    42416687
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@571 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/products/views/popup_edit_price.php
+++ www/app/controllers/products/views/popup_edit_price.php
@@ -19,48 +19,36 @@ $edit = '#!products-edit--prod_id-'.$core->data['prod_id'];
 $edit .= '-tabautoswitch_producttabs-3';
 $this->pricing_advanced_rules()->js();
 ?>
-<form name="pricing_advanced_rules" action="/products/popup_save_price" onsubmit="return core.submit('/products/save_price',this);">
+<form name="pricing_advanced_rules" class="form-horizontal" action="/products/popup_save_price" onsubmit="return core.submit('/products/save_price',this);">
 	<fieldset id="editPrice">
 		<legend>Price Info</legend>
-		<table class="form">
-			<tr>
-				<td class="label">Hub</td>
-				<td class="value">
-					<select name="domain_id">
-						<option value="0">Everywhere</option>
-						<?=core_ui::options($domains,$price['domain_id'],'domain_id','name')?>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td class="label">Customer</td>
-				<td class="value">
-					<select name="org_id">
-						<option value="0">Everyone</option>
-						<?=core_ui::options($orgs,$price['org_id'],'org_id','name')?>
-					</select>
-				</td>
-			</tr>
-			<?if(!lo3::is_admin() && $seller['feature_sellers_enter_price_without_fees'] == 1){?>
-			<tr>
-				<td class="label">Seller Net Price</td>
-				<td class="value"><input type="text" name="seller_net_price" onkeyup="product.syncPrices(this,'price');" value="<?=core_format::price(core_format::parse_price($price['price']) - (core_format::parse_price($price['price']) * ($seller['total_fees']/100)))?>" /></td>
-			</tr>
-			<tr>
-				<td class="label">Sales Price</td>
-				<td class="value"><input type="text" name="price" onkeyup="product.syncPrices(this,'seller_net_price');" value="<?=$price['price']?>" /></td>
-			</tr>
-			<?}else{?>
-			<tr>
-				<td class="label">Price</td>
-				<td class="value"><input type="text" name="price" value="<?=$price['price']?>" /></td>
-			</tr>
-			<?}?>
-			<tr>
-				<td class="label">Minimum Quantity</td>
-				<td class="value"><input type="text" name="min_qty" value="<?=floatval($price['min_qty'])?>" /></td>
-			</tr>
-		</table>
+		<?=core_form::input_select('Market','domain_id',$price['domain_id'],$domains,array(
+			'text_column'=>'name',
+			'value_column'=>'domain_id',
+			'default_show'=>true,
+			'default_text'=>'Everywhere',
+		))?>
+		<?=core_form::input_select('Customer','org_id',$price['org_id'],$orgs,array(
+			'text_column'=>'name',
+			'value_column'=>'org_id',
+			'default_show'=>true,
+			'default_text'=>'Everyone',
+		))?>
+		
+		
+		<?if(!lo3::is_admin() && $seller['feature_sellers_enter_price_without_fees'] == 1){?>
+			<?=core_form::input_text('Seller Net Price','seller_net_price',core_format::price(core_format::parse_price($price['price']) - (core_format::parse_price($price['price']) * ($seller['total_fees']/100))),array(
+				'onkeyup'=>'product.syncPrices(this,\'price\');',
+			))?>
+			<?=core_form::input_text('Sales Price','price',$price['price'],array(
+				'onkeyup'=>'product.syncPrices(this,\'seller_net_price\');',
+			))?>
+		<?}else{?>
+		<?=core_form::input_text('Sales Price','price',$price['price'])?>
+		<?}?>
+		<?=core_form::input_text('Minimum Quantity','min_qty',floatval($price['min_qty']))?>
+		
+		
 		<input type="hidden" name="price_id" value="<?=$price['price_id']?>" />
 		<input type="hidden" name="prod_id" value="<?=$price['prod_id']?>" />
 		<input type="hidden" name="call_method" value="popup" />
@@ -71,9 +59,9 @@ $this->pricing_advanced_rules()->js();
 			to view your pricing information.
 		</div>
 		
-		<div class="buttonset">
-			<input type="button" onclick="$('#edit_popup').fadeOut('fast');" class="button_primary" value="cancel" />
-			<input type="submit" class="button_primary" value="save" />
+		<div class="form-actions pull-right">
+			<input type="button" onclick="$('#edit_popup').fadeOut('fast');" class="btn btn-warning" value="cancel" />
+			<input type="submit" class="btn btn-primary" value="save" />
 		</div>		
 	</fieldset>
 </form>
