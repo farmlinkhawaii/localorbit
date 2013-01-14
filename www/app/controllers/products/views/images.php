87bb06c2a013e382f0096b7e1dcbc6c4fe3c7c0f
lo-svn <lo-svn>
2013-01-14 22:12:12 +0000

    42416687
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@579 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/products/views/images.php
+++ www/app/controllers/products/views/images.php
@@ -20,11 +20,9 @@ else
 ?>
 <div class="buttonset">
 	<input type="file" id="new_image" name="new_image" value="" />
-	<input type="button" class="button_secondary" value="Upload" onclick="core.ui.uploadFrame(document.prodForm,'uploadArea','product.refreshImage(<?=intval($images[0]['pimg_id'])?>,{params});','app/products/save_image','new_image');" />
-	<input type="button" id="removeLogo" class="button_secondary" value="Remove Image" onclick="core.doRequest('/products/remove_image',{'pimg_id':document.prodForm.old_pimg_id.value});" /> 
-
-
+	<input type="button" class="btn btn-info" value="Upload" onclick="core.ui.uploadFrame(document.prodForm,'uploadArea','product.refreshImage(<?=intval($images[0]['pimg_id'])?>,{params});','app/products/save_image','new_image');" />
+	<input type="button" id="removeLogo" class="btn btn-warning" value="Remove Image" onclick="core.doRequest('/products/remove_image',{'pimg_id':document.prodForm.old_pimg_id.value});" /> 
 </div>
 <input type="hidden" name="old_pimg_id" value="<?=$images[0]['pimg_id']?>" />
-<iframe name="uploadArea" id="uploadArea" width="300" height="20" style="color:#fff;background-color:#fff;overflow:hidden;"></iframe>
+<iframe name="uploadArea" id="uploadArea" width="300" height="20" style="border-width: 0px;color:#fff;background-color:#fff;overflow:hidden;"></iframe>
 <div id="output"></div>
