12fa17f8a17b987e387eaf3b6e5e72a45c383688
lo-svn <lo-svn>
2013-01-14 22:19:20 +0000

    42421845 Seller Profile - Photo upload is not formatted with new UI
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@580 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/organizations/views/profile.php
+++ www/app/controllers/organizations/views/profile.php
@@ -11,44 +11,43 @@ list($has_image,$webpath,$filepath) = $data->get_image();
 if($data['allow_sell'] == 1)
 {
 	?>
+	
 	<div class="tabarea" id="orgtabs-a<?=$tab_id?>">
-		<table class="form">
-			<tr>
-				<td class="value" colspan="2">
-					<div id="imgContainer">
-						<img id="orgImg" src="<?=$webpath?>" />
-					</div>
-					<div class="buttonset">
-						<input type="file" name="new_image" value="" />
-						<input type="button" class="button_secondary" value="Upload" onclick="core.ui.uploadFrame(document.organizationsForm,'uploadArea','org.refreshImage({params});','app/organizations/save_image');" />
-						<input type="button" id="removeLogo" class="button_secondary" value="Remove Image" onclick="core.doRequest('/organizations/remove_image',{'org_id':<?=$data['org_id']?>});" /> 
-					</div>
-					Note: images can not be larger than 400 pixels wide by 400 pixels tall.<br />
-					<iframe name="uploadArea" id="uploadArea" width="300" height="20" style="color:#fff;background-color:#fff;overflow:hidden;"></iframe>
-				</td>
-			</tr>
-			<tr>
-				<td class="label">&nbsp;</td>
-				<td class="value"><?=core_ui::checkdiv('public_profile','Show my profile on Our Sellers page.',($data['public_profile'] == 1))?></td>
-			</tr>
+		<?=core_form::input_textarea('Who','profile',$data,array(
+			'sublabel'=>'Your organization\'s story',
+			'required'=>true,
+			'rows'=>5,
+			'cols'=>50,
+			'info'=>'Customers can view this field when viewing your farm profile or a product. Additionally, you can override this on a per-product basis.',
+		))?>
 
 
-			<?=core_form::input_textarea('Who','profile',$data,array(
-				'sublabel'=>'Your organization\'s story',
-				'required'=>true,
-				'rows'=>5,
-				'cols'=>50,
-				'info'=>'Customers can view this field when viewing your farm profile or a product. Additionally, you can override this on a per-product basis.',
-			))?>
-
-
-			<?=core_form::input_textarea('How','product_how',$data,array(
-				'sublabel'=>'Your products\' story',
-				'required'=>true,
-				'rows'=>5,
-				'cols'=>50,
-				'info'=>'Every product has a description of how it is made. The value of this field will be used for your default Long How. Additionally, you can override this on a per-product basis.',
-			))?>
-		</table>
+		<?=core_form::input_textarea('How','product_how',$data,array(
+			'sublabel'=>'Your products\' story',
+			'required'=>true,
+			'rows'=>5,
+			'cols'=>50,
+			'info'=>'Every product has a description of how it is made. The value of this field will be used for your default Long How. Additionally, you can override this on a per-product basis.',
+		))?>
+		
+		<div class="control-group">
+			<label for="profile" class="control-label">  
+				Photo
+				<span class="help-block">Note: images can not be larger than 400 pixels wide by 400 pixels tall.</span>
+			</label>
+			<div class="controls">
+				<div id="imgContainer" class="float_left">
+					<img id="orgImg" src="<?=$webpath?>" class="img_upload_view" />
+				</div>
+				<div class="buttonset">
+					<input type="file" name="new_image" class="btn" value="" /><br />
+					<input type="button" class="btn button_secondary" value="Upload" onclick="core.ui.uploadFrame(document.organizationsForm,'uploadArea','org.refreshImage({params});','app/organizations/save_image');" /><br />
+					<input type="button" id="removeLogo" class="btn button_secondary" value="Remove Image" onclick="core.doRequest('/organizations/remove_image',{'org_id':<?=$data['org_id']?>});" /> <br />
+				</div>
+				
+				<iframe name="uploadArea" id="uploadArea" width="300" height="20" style="color:#fff;background-color:#fff;overflow:hidden;"></iframe><br />
+				<?=core_ui::checkdiv('public_profile','Show my profile on Our Sellers page.',($data['public_profile'] == 1))?>				
+			</div>
+		</div>
 	</div>
 <?}?>
