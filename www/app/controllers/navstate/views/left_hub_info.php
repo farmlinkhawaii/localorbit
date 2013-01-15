3b04ebd8bacfd8980b0e414204c618a12e05a798
lo-svn <lo-svn>
2013-01-15 04:04:48 +0000

    42533995
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@602 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/navstate/views/left_hub_info.php
+++ www/app/controllers/navstate/views/left_hub_info.php
@@ -49,9 +49,6 @@ foreach($delivs as $deliv)
 }
 ?>
 <?}?>
-
-
-<a class="twitter-timeline span3" href="https://twitter.com/EasternMarket" data-widget-id="286885721258725376">Tweets by @EasternMarket</a>
 <p>&nbsp;</p>
 <?
 core::js('!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");');
