1f00483047794b69e59e2ac1e74471b7fae9b192
lo-svn <lo-svn>
2013-01-22 19:04:39 +0000

    42912979
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@674 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/render_product_description.php
+++ www/app/controllers/catalog/views/render_product_description.php
@@ -6,20 +6,23 @@ $seller = $core->view[1];
 $description = $prod['short_description'];
 $farm_name = $prod['org_name'];
 
-$description_popup = $prod['who'];
+$description_popup = trim($prod['who']);
 if($description_popup == '')
-	$description_popup = $seller['profile'];
-$description_popup .= '<p><a class="btn btn-small btn-info pull-right" href="#!sellers-oursellers--org_id-'.$prod['org_id'].'"><i class="icon-arrow-right" /> Learn More</a></p>';
-$description_popup = htmlentities($description_popup);
-
+	$description_popup = trim($seller['profile']);
+if (!empty($description_popup))
+{
+	$description_popup .= '<p><a class="btn btn-small btn-info pull-right" href="#!sellers-oursellers--org_id-'.$prod['org_id'].'"><i class="icon-arrow-right" /> Learn More</a></p>';
+	$description_popup = htmlentities($description_popup);
+}
 
 $how_popup = $prod['how'];
 if($how_popup == '')
 	$how_popup = $seller['product_how'];
-if($how_popup !=='')
+if(!empty($how_popup))
+{
 	$how_popup .= '<p><a class="btn btn-small btn-info pull-right" href="#!catalog-view_product--prod_id-'.$prod['prod_id'].'"><i class="icon-arrow-right" /> Learn More</a></p>';
-
-$how_popup = htmlentities($how_popup);
+	$how_popup = htmlentities($how_popup);
+}
 
 
 
@@ -30,17 +33,29 @@ $how_popup = htmlentities($how_popup);
 
 <a href="#!catalog-view_product--prod_id-<?=$prod['prod_id']?>"><?=$prod['name']?></a>
 <br />
-<?=$description?>			
+<?=$description?>
 <br />
 <small class="whowhatwhere">
+	<?
+	if (!empty($description_popup))
+	{
+	?>
 	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="" data-content="<?=$description_popup?>">
 		<i class="icon icon-binoculars" />
 		<?=$farm_name?>
 	</a>&nbsp;
+	<?
+	}
+	if (!empty($how_popup))
+	{
+	?>
 	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="" data-content="<?=$how_popup?>">
 		<i class="icon icon-eye" />
 		How
 	</a>&nbsp;
+	<?
+	}
+	?>
 	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="<?=$prod['city']?>, <?=$prod['code']?>" data-content="<?= htmlspecialchars('<img src="//maps.googleapis.com/maps/api/staticmap?center=' . $prod['latitude'] . ',' . $prod['longitude'] . '&zoom=7&size=310x225&sensor=false&markers=size:small%7Ccolor:white%7C' . $prod['latitude'] . ',' . $prod['longitude'] . '" />'); ?>">
 		<i class="icon icon-direction" /> Where
 	</a>
