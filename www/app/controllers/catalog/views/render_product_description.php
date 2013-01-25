e4108743d54b1be22c3cca889bea3dd8599521ba
lo-svn <lo-svn>
2013-01-25 22:05:04 +0000

    42341785
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@700 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/render_product_description.php
+++ www/app/controllers/catalog/views/render_product_description.php
@@ -10,7 +10,7 @@ $description_popup = trim($prod['product_who']);
 
 if (!empty($long_description))
 {
-	$long_description = htmlentities($long_description . '<p><a class="btn btn-small btn-info pull-right learnMore" href="#!catalog-view_product--prod_id-'.$prod['prod_id'].'">Learn More</a></p><i data-dismiss="clickover" class="icon-close"/>');
+	$long_description = htmlentities($long_description . '<p><a class="btn btn-small btn-info pull-right learnMore span3" href="#!catalog-view_product--prod_id-'.$prod['prod_id'].'">Learn More</a></p><i data-dismiss="clickover" class="icon-close"/>');
 }
 //if($description_popup == '')
 if (!empty($description_popup))
@@ -51,7 +51,7 @@ if (!empty($long_description) && trim($prod['description']) !== trim($descriptio
 //	if (!empty($description_popup))
 //	{
 	?>
-	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="" data-content="<?=$description_popup?>">
+	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="Who" data-content="<?=$description_popup?>">
 		<i class="icon icon-binoculars" />
 		<?=$farm_name?>
 	</a>&nbsp;
@@ -60,14 +60,14 @@ if (!empty($long_description) && trim($prod['description']) !== trim($descriptio
 	if (!empty($how_popup))
 	{
 	?>
-	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="" data-content="<?=$how_popup?>">
+	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="How" data-content="<?=$how_popup?>">
 		<i class="icon icon-eye" />
 		How
 	</a>&nbsp;
 	<?
 	}
 	?>
-	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="<?=$prod['city']?>, <?=$prod['code']?>" data-content="<?= htmlspecialchars('<img src="//maps.googleapis.com/maps/api/staticmap?center=' . $prod['latitude'] . ',' . $prod['longitude'] . '&zoom=7&size=310x225&sensor=false&markers=size:small%7Ccolor:white%7C' . $prod['latitude'] . ',' . $prod['longitude'] . '" /><i data-dismiss="clickover" class="icon-close"/>'); ?>">
-		<i class="icon icon-flag" /> Where
+	<a href="" onclick="return false;" rel="clickover" data-placement="bottom" data-title="<?=$prod['city']?>, <?=$prod['code']?>" data-content="<?= htmlspecialchars('<img src="//maps.googleapis.com/maps/api/staticmap?center=' . $prod['latitude'] . ',' . $prod['longitude'] . '&zoom=7&size=310x225&sensor=false&markers=icon:http://chart.apis.google.com/chart?chst=d_map_pin_icon%26chld=glyphish_flag%257CFFFFFF%7Csize:small%7Ccolor:white%7C' . $prod['latitude'] . ',' . $prod['longitude'] . '" /><i data-dismiss="clickover" class="icon-close"/>'); ?>">
+		<i class="icon icon-direction" /> Where
 	</a>
 </small>
