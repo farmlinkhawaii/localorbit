15be55b5fbf3a64a15c01a3868ac749e06aa6c10
lo-svn <lo-svn>
2013-01-28 18:40:41 +0000

    42338685
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@711 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/market/views/branding.php
+++ www/app/controllers/market/views/branding.php
@@ -1,13 +1,7 @@
 <?php global $data; ?>
 <style type="text/css">
 <?
-	$font_list = array();
 	$fonts = core::model('fonts')->collection();
-	foreach ($fonts as $font) {
-		list($font_label) = explode(',', $font['font_name']);
-		$font_list[] = str_replace(' ', '+', str_replace('\'', '', $font_label));
-	}
-	$font_list = implode('|', $font_list);
 ?>
 	@import url(http://fonts.googleapis.com/css?family=<?=$font_list?>);
 </style>
