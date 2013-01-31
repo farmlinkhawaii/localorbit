f0d3bf5e413e4d3f8453c0cd6686ef111f4fb3c5
lo-svn <lo-svn>
2013-01-31 22:14:48 +0000

    moved to 00n file naming
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@760 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- db/patches/4.01/3_42843881.sql
+++
@@ -1,3 +0,0 @@
-UPDATE phrases
-SET default_value = 'I have read and agree to the Terms of Service'
-WHERE default_value = 'I have read and agree to the <a href="#!misc-tos" target="_blank">Terms of Service</a>'
