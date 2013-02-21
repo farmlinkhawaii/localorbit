0d01bacc895328e12975b461494c20a6366e9b86
lo-svn <lo-svn>
2013-02-21 22:23:16 +0000

    fixing bugs with fully managed ach queue
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/121206-uiredesign@884 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- bin/ach/fully_managed_seller_payables.php
+++ bin/ach/fully_managed_seller_payables.php
@@ -16,31 +16,26 @@ $sql = '
 	select 
 	p.payable_id,p.amount,p.payable_type_id,p.invoice_id,
 	lfo.org_id as seller_org_id,o.name,
-	opm.*,d1.payable_org_id,lo.domain_id,
-	sp.payable_id as seller_payable_id,
-	sp.from_org_id as seller_from_org_id,
-	sp.to_org_id as seller_to_org_id
+	opm.*,lo.domain_id
 	from payables p
-	inner join payables sp on (sp.parent_obj_id=p.parent_obj_id and sp.payable_type_id=2 and sp.from_org_id=p.to_org_id)
 	inner join lo_fulfillment_order lfo on (lfo.lo_foid=p.parent_obj_id)
 	inner join lo_order_line_item loi on (lfo.lo_foid=loi.lo_foid)
 	inner join lo_order lo on (lo.lo_oid=loi.lo_oid)
+    inner join domains d on (d.domain_id=lo.domain_id)
 	inner join organizations o on (lfo.org_id=o.org_id)
-	inner join domains d1 on (lo.domain_id=d1.domain_id)
-	inner join organization_payment_methods opm on (d1.opm_id = opm.opm_id)
+	inner join organization_payment_methods opm on (o.opm_id = opm.opm_id)
 		
 	where p.payable_type_id=2
 	and   p.invoice_id is null
 	and   p.from_org_id=1
 	and   lo.ldstat_id=4
-	and   lo.payment_method in (\'ach\',\'paypal\')
-	and   d1.seller_payer = \'hub\'
+    and   lo.lbps_id=2
+	and   d.seller_payer = \'lo\'
 	group by lfo.lo_foid
 ';
 
 $payables = new core_collection($sql);
 $sellers = array();
-$domains = array();
 
 # first build a structure that groups all the payables by seller in order to create invoices
 # we also need to total up things by domain.payable_org_id to send the money to the hub
@@ -51,26 +46,13 @@ foreach($payables as $payable)
 		$sellers[$payable['seller_org_id']] = array(
 			'total'=>0,
 			'name'=>$payable['name'],
-			'payables'=>array(),
-		);
-	}
-	
-	if(!is_array($domains[$payable['payable_org_id']]))
-	{
-		$domains[$payable['payable_org_id']] = array(
-			'total'=>0,
 			'account'=>$payable,
 			'payables'=>array(),
 		);
 	}
 	
-	
 	$sellers[$payable['seller_org_id']]['total']  += $payable['amount'];
 	$sellers[$payable['seller_org_id']]['payables'][] = $payable;
-	
-	$domains[$payable['payable_org_id']]['total'] += $payable['amount'];
-	$domains[$payable['payable_org_id']]['payables'][] = $payable;
-	
 }
 
 # output the payment structure
@@ -85,34 +67,27 @@ foreach($sellers as $org_id=>$seller)
 	echo("\n");
 }
 
-echo("-------------------\n");
-echo("Payments to MMs: \n");
-foreach($domains as $org_id=>$info)
-{
-	echo("we need to pay org ".$org_id." ".$info['total']."\n");
-	
-	foreach($info['payables'] as $payable)
-	{
-		echo($payable['payable_id'].' ');
-	}
-	echo("\n");
-}
+
 echo("-------------------\n");
 
 
 if($actually_do_payment)
 {
 	# pay the Market Managers for their orders
-	foreach($domains as $org_id=>$info)
+	foreach($sellers as $org_id=>$info)
 	{
 		$opm = core::model('organization_payment_methods');
 		$core->data = $info['account'];
 		$opm->import_fields('opm_id','org_id','payment_method_id','label','name_on_account','nbr1','nbr2');
 		
-		$trace = 'LO-SMSP-'.$org_id.'-'.date('Ymd');
+		
+		#print_r($opm->__data);
+		
+		#exit();
+		$trace = 'LO-FMSP-'.$org_id.'-'.date('Ymd');
 		$trace .= '-'.time();
 		
-		$result = $opm->make_payment($trace,'Seller payments on '.date('M, d Y'),$info['total']);
+		$result = $opm->make_payment($trace,'Payments for products on '.date('M, d Y'),(-1) * $info['total']);
 		
 		if($result)
 		{
@@ -147,30 +122,6 @@ if($actually_do_payment)
 			$xpi->save();
 		}
 		#print_r($info);
-	}
-	
-	# now, loop through all the sellers and invoice their payables
-	foreach($sellers as $org_id=>$seller)
-	{
-		
-		if($seller['total'] > 0)
-		{
-			# create teh invoice
-			$invoice = core::model('invoices');
-			$invoice['from_org_id'] = $payable['payable_org_id'];
-			$invoice['to_org_id'] = $org_id;
-			$invoice['amount'] = $seller['total'];
-			$invoice['due_date'] = date('Y-m-d H:i:s',time() + (7*86400));
-			$invoice->save();
-			
-			foreach($seller['payables'] as $payable)
-			{
-				$payable_obj = core::model('payables');
-				$payable_obj->load($payable['seller_payable_id']);
-				$payable_obj['invoice_id'] = $invoice['invoice_id'];
-				$payable_obj->save();
-			}
-		}
 	}
 }
 
