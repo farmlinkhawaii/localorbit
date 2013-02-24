ac5588137be2a9724c19df48aba0148887177414
localorbit-chad <chad@localorb.it>
2013-02-24 19:19:07 +0000

    Added site checks to index.php
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/home-wp-fix@904 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/index.php
+++ www/index.php
@@ -1,4 +1,19 @@
 <?php
+if(in_array(strtolower($_SERVER['HTTP_HOST']),array('localorbit.com','localorbit.it','localorbit.org','www.localorbit.com','www.localorbit.org')))
+{
+	header('Location: http://www.localorb.it/');
+	exit();
+}
+
+$prtcl = ($_SERVER['SERVER_PORT'] == 80)?'http://':'https://';
+
+if(isset($_REQUEST['_escaped_fragment_']))
+{
+	define('__CORE_AJAX_OUTPUT__',false);
+	include('app/index.php');
+	exit();
+}
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
