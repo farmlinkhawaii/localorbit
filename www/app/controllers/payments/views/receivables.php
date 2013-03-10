23d592c33e93495df0acfa306b89e55162dff8a0
lo-svn <lo-svn>
2013-03-10 01:46:56 +0000

    45898319
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/4.0@1140 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/payments/views/receivables.php
+++ www/app/controllers/payments/views/receivables.php
@@ -25,7 +25,7 @@ $invoices->add_formatter('payment_link_formatter');
 $invoices->add_formatter('payment_direction_formatter');
 $invoices_table = new core_datatable('receivables','payments/receivables',$invoices);
 $invoices_table = payments__add_standard_filters($invoices_table,'invoices');
-$invoices_table->add(new core_datacolumn('invoice_id','Description',true,'22%',			'<b>I-{invoice_id}</b><br />{description_html}','{description}','{description}'));
+$invoices_table->add(new core_datacolumn('invoice_id','Description',true,'22%',			'{description_html}','{description}','{description}'));
 $invoices_table->add(new core_datacolumn('from_org_name','Payment Info',false,'40%','{direction_info}','{from_org_name}','{from_org_name}'));
 $invoices_table->add(new core_datacolumn('due_date','Due Date',true,'20%','{due_date}','{due_date}','{due_date}'));
 $invoices_table->add(new core_datacolumn('amount_due','Amount Due',true,'14%',			'{amount_due}','{amount_due}','{amount_due}'));
