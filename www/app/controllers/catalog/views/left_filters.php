ec80d95a747b2f83b6cd0909bb8fb0c7277164e7
Mike Ratanatharathorn <mike@localorb.it>
2013-03-21 23:35:24 +0000

    4.0.5.1 release
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/4.0.5.1@1272 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/controllers/catalog/views/left_filters.php
+++ www/app/controllers/catalog/views/left_filters.php
@@ -5,8 +5,10 @@ $cats = $core->view[0];
 $sellers = $core->view[1];
 //$delivs = $core->view[2];
 $days = $core->view[2];
+$addresses = $core->view[3];
 $hashUrl = $core->view[3]?'true':'false';
 
+#print_r($addresses);
 if($core->data['cart'] == 'yes')
 	core::js('$(\'#cartFilterCheck\').prop(\'checked\',true);core.catalog.setFilter(\'cartOnly\',true);');
 ?>
@@ -39,10 +41,17 @@ if (count($days) > 1)
 	{
 		$name = core_format::date($time, 'shorter-weekday');
 		$dd_ids = implode('_', array_keys($day));
-		list($type, $time) = explode('-', $key);
+		#print_r($dd_ids );
+		#print_r(explode('-', $key));
+		list($type, $time,$deliv_address_id,$pickup_address_id) = explode('-', $key);
+		$final_address = ($deliv_address_id == 0)?$deliv_address_id:$pickup_address_id;
+		$final_address = ($final_address == 0)?'directly to you':' at ' .$addresses[$final_address][0]['formatted_address'];
 		?>
-		<li class="filter dd" id="filter_dd_<?=$dd_ids?>"><a href="#!catalog-shop#dd<?=$dd_ids?>" onclick="core.catalog.setFilter('dd','<?=$dd_ids?>'); return <?=$hashUrl?>;">
-		<?=$type?> <?=core_format::date($time, 'shorter-weekday',false)?></a>
+		<li class="filter dd" id="filter_dd_<?=$dd_ids?>"><a href="#!catalog-shop" onclick="core.catalog.setFilter('dd','<?=$dd_ids?>'); return <?=$hashUrl?>;">
+		<?=$type?> <?=core_format::date($time, 'shorter-weekday',false)?>
+		<br />
+		<?=$final_address?>
+		</a>
 		<input type="hidden" id="filtercheck_<?=$dd_ids?>" class="filtercheck" checked="checked" /></li>
 		<?
 	}
@@ -60,7 +69,7 @@ foreach($sellers as $seller)
 	if($seller[0]['name'] != '')
 	{
 	?>
-	<li class="filter seller" id="filter_org_<?=$seller[0]['org_id']?>"><a href="#!catalog-shop#seller=<?=$seller[0]['org_id']?>" onclick="core.catalog.setFilter('seller',<?=$seller[0]['org_id']?>); return <?=$hashUrl?>;"><?=$seller[0]['name']?></a>
+	<li class="filter seller" id="filter_org_<?=$seller[0]['org_id']?>"><a href="#!catalog-shop" onclick="core.catalog.setFilter('seller',<?=$seller[0]['org_id']?>); return <?=$hashUrl?>;"><?=$seller[0]['name']?></a>
 	<input type="hidden" id="filtercheck_<?=$cat[0]['cat_id']?>" class="filtercheck" checked="checked" /></li>
 	<?
 	}
@@ -79,7 +88,7 @@ foreach($cats->roots as $root)
 	$cat = $cats->by_id[$root];
 ?>
 <li class="filter category" data-name="<?=$cat[0]['cat_name']?>" id="filter_cat_<?=$cat[0]['cat_id']?>">
-	<a href="#!catalog-shop#cat1=<?=$cat[0]['cat_id']?>" onclick="core.catalog.setFilter('cat1',<?=$cat[0]['cat_id']?>);  return <?=$hashUrl?>;">
+	<a href="#!catalog-shop" onclick="core.catalog.setFilter('cat1',<?=$cat[0]['cat_id']?>);  return <?=$hashUrl?>;">
 		<!--<input type="hidden" id="filtercheck_<?=$cat[0]['cat_id']?>" class="filtercheck" checked="checked" />-->
 		<?=$cat[0]['cat_name']?>
 	</a>
