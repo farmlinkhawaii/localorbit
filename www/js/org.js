a092f51b0362651f872bb9f29c86c89f76c64848
Mike Ratanatharathorn <mike@localorb.it>
2013-04-15 18:10:29 +0000

    47488747
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/4.0.6@1382 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/js/org.js
+++ www/js/org.js
@@ -148,4 +148,17 @@ org.savePaymentMethod=function(formObj){
 
 org.cancelPaymentChanges=function(){
 	$('#paymentsTable,#main_save_buttons,#addPaymentButton,#editPaymentMethod').toggle();
+<<<<<<< .working
+=======
+}
+
+org.securityAssurance=function(refObj){
+	
+	var pos = $(refObj).offset(); 
+	$('#edit_popup').css( { 
+		'left': (pos.left - 160)+'px', 
+		'top': (pos.top - 60)+'px'
+	});
+	core.doRequest('/organizations/security_assurance',{});
+>>>>>>> .merge-right.r1381
 }
