9ba491c95919fd2c5a8440d9fbff167da90e58ce
Mike Ratanatharathorn <mike@localorb.it>
2013-04-25 21:45:24 +0000

    48771907
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/4.0.6.1@1519 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/homepage/features.php
+++ www/homepage/features.php
@@ -91,7 +91,7 @@ popupWindow = window.open(url,winName,settings)
 				<tr>
 					<td>
 						<img src="/homepage/img/icon_link_small.png" class="home_icon" /><h3 class="home_header">Efficient Logistics</h3>
-						<div class="home_paragraph">
+						<div class="home_paragraph" style="height: 115px;">
 							Generate pick lists and packing slips. Automated emails remind buyers and sellers about 
 							upcoming deliveries. Producers maintain their brand throughout the process. </div>
 						<img class="thumb" onclick="doFeaturePopup('logistics',975)" src="/homepage/img/features/logistics_thumb.png" />
@@ -100,7 +100,7 @@ popupWindow = window.open(url,winName,settings)
 					</td>
 					<td>
 						<img src="/homepage/img/icon_megaphone.png" class="home_icon" /><h3 class="home_header">Integrated Marketing</h3>
-						 <div class="home_paragraph">
+						 <div class="home_paragraph" style="height: 115px;">
 						 Manage everything in one place: create discounts, promotions, <br>micro-blogs, newsletters and Fresh Sheets 
 						 in minutes. Integrate your facebook or twitter feeds.
 						</div>
@@ -112,7 +112,7 @@ popupWindow = window.open(url,winName,settings)
 					</td>
 					<td>
 						<img src="/homepage/img/icon_bar_graph.png" class="home_icon" /><h3 class="home_header">Reporting and Analysis</h3>
-						<div class="home_paragraph">
+						<div class="home_paragraph" style="height: 115px;">
 							Power your business with knowledge. Sophisticated, easy-to-use reports let you track, learn from and improve your business weekly, monthly and year-over-year.
 						</div>
 					
