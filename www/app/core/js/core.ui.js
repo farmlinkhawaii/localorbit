da7be637073185f908fb69ea210d871764a5379c
Mike Ratanatharathorn <mike@localorb.it>
2013-06-24 16:51:47 +0000

    fixing IE issue, merging with older data table code
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/4.0.7.1@2125 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/app/core/js/core.ui.js
+++ www/app/core/js/core.ui.js
@@ -298,15 +298,63 @@ core.ui.dataTable.prototype.loadData=function(format){
 	}
 	//alert('loading data: '+url);
 }
- 
+
 core.ui.dataTable.prototype.insertData=function(jsondata){
-	
+	//alert('inserting data');
+	this.adjustRows(jsondata.size,jsondata.data.length);
 	this.adjustPager(jsondata.page,jsondata.max_page);
-	$('#dt_'+this.name+' > tbody').html(core.base64_decode(jsondata.data));
-	$('#dt_'+this.name+' > thead > tr.dt_nodata')[((jsondata.data == '')?'show':'hide')]();
-	$('#dt_'+this.name+' > thead > tr.dt_columns,#dt_'+this.name+' > tbody')[((jsondata.data == '')?'hide':'show')]();
+	for (var i = 0; i < jsondata.data.length; i++){
+		for (var j = 0; j < jsondata.data[i].length; j++){
+			//alert('trying to set '+'#dt_'+this.name+'_'+i+'_'+j);
+			$('#dt_'+this.name+'_'+i+'_'+j).html(core.base64_decode(jsondata.data[i][j]));
+		}
+		//alert(core.base64_decode(jsondata.data[i][1]));
+	}
+	if(jsondata.data.length>0){
+		$('#dt_'+this.name+'_nodata').hide();
+		$('#dt_'+this.name+'_columns').show();
+	}else{
+		$('#dt_'+this.name+'_nodata').show();
+		$('#dt_'+this.name+'_columns').hide();
+	}
 }
 
+
+
+core.ui.dataTable.prototype.adjustRows=function(newsize,newLength){
+	//alert('need to show '+newsize+'.\nthe new data length is '+newLength+'.\nwe have '+this.renderedSize+' rows rendered.\n we have '+this.displaySize+' displayed');
+	if(this.displaySize > (newLength)){
+		//alert('gotta turn rows off');
+		for (var i = (this.displaySize); i > newLength; i--){
+			//alert('turning off '+'#dt_'+this.name+'_'+(i - 1));
+			$('#dt_'+this.name+'_'+(new String(i - 1))).hide();
+			$('#dt_'+this.name+'_'+(new String(i - 1))).children().html('&nbsp;');
+			this.displaySize--;
+		}
+	}
+	if (this.renderedSize < newLength){
+		var html = '';
+		for (var i = (this.renderedSize); i < newLength; i++)
+		{
+			html += '<tr id="dt_'+this.name+'_'+i+'" class="dt'+(i%2)+'">';
+			for (var j= 0; j < this.colCount; j++)
+				html += '<td class="dt" id="dt_'+this.name+'_'+i+'_'+j+'">&nbsp;</td>';
+			html += '</tr>';
+			this.displaySize++;
+		}
+		this.renderedSize = newLength;
+		$('#dt_'+this.name+' tr:last').before(html);
+		// add more rows
+	}
+	//alert('need to show '+newsize+'.\nthe new data length is '+newLength+'.\nwe have '+this.renderedSize+' rows rendered.\n we have '+this.displaySize+' displayed');
+
+	if(this.displaySize < newLength)
+	{
+		for (var i = this.displaySize; i < newLength; i++)
+			$('#dt_'+this.name+'_'+(i)).show();
+		this.displaySize = (newLength);
+	}
+}
 
 core.ui.dataTable.prototype.adjustPager=function(page,maxPage){
 	var selector = $('#dt_'+this.name+'_pager');
@@ -333,19 +381,19 @@ core.ui.dataTable.updateFilter=function(dtFilterName,filtValue,parseDate){
 	if(parseDate === true)
 	{
 		var months = {
-				'Jan':'01',
-				'Feb':'02',
-				'Mar':'03',
-				'Apr':'04',
-				'May':'05',
-				'Jun':'06',
-				'Jul':'07',
-				'Aug':'08',
-				'Sep':'09',
-				'Oct':'10',
-				'Nov':'11',
-				'Dec':'12'
-			};
+			'January':'01',
+			'February':'02',
+			'March':'03',
+			'April':'04',
+			'May':'05',
+			'June':'06',
+			'July':'07',
+			'August':'08',
+			'September':'09',
+			'October':'10',
+			'November':'11',
+			'December':'12'
+		};
 		var parts = new String(filtValue).split(/\s/);
 		parts[1] = parseInt(new String(parts[1]).replace(',',''));
 		if(parts[1] < 10) {
