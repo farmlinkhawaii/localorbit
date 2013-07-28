2ce0b79cd280920ad25ac936f22fa7839f6a5955
Mike Ratanatharathorn <mike@localorb.it>
2013-07-28 15:17:52 +0000

    53501647
    git-svn-id: svn+ssh://lo-svn/opt/localorbit/lo3/branches/4.0.7.3@2555 e1dfb558-8c25-43e7-bb2c-da0e82dfb25a

--- www/js/catalog.js
+++ www/js/catalog.js
@@ -104,7 +104,7 @@ core.catalog.setFilter=function(type,id,parentId,updateListing){
 			//$('.filter_dd').removeClass('active');
 			if(core.catalog.filters.dd != 0){
 				core.log('removing class from '+core.catalog.filters.dd);
-				$('#filter_dd_'+core.catalog.filters.dd).parent().removeClass('active');
+				$('#filter_dd_'+core.catalog.filters.dd+',#filter_ddpopup_'+core.catalog.filters.dd).parent().removeClass('active');
 			}
 			var oldDD = core.catalog.filters.dd;
 			core.catalog.filters.dd = (core.catalog.filters.dd == id)?0:id;
@@ -115,7 +115,7 @@ core.catalog.setFilter=function(type,id,parentId,updateListing){
 				// if we were turning off the filter, turn all on
 				//core.log('adding class');
 				core.catalog.resetDeliveryDateChange();
-				$('#filter_dd_'+core.catalog.filters.dd).parent().addClass('active');
+				$('#filter_dd_'+core.catalog.filters.dd+',#filter_ddpopup_'+core.catalog.filters.dd).parent().addClass('active');
 				core.catalog.confirmDeliveryDateChange(true);
 				//$('.prodDd').val(id);
 			}else{
@@ -397,7 +397,7 @@ core.catalog.updateListing=function(){
 }
 
 core.catalog.matchesDeliveryDay=function(deliveryDays, product) {
-	var a = deliveryDays.split('_').sort();
+	var a = new String(deliveryDays).split('_').sort();
 	var b = product.dd_ids.split(',').sort();
 	while( a.length > 0 && b.length > 0 )
 	{
@@ -411,13 +411,14 @@ core.catalog.matchesDeliveryDay=function(deliveryDays, product) {
 	return false;
 }
 
-core.catalog.initCatalog=function(cartOnly){
+core.catalog.initCatalog=function(cartOnly,ddIdFilter){
 	core.catalog['filters']['seller'] = 0;
 	core.catalog['filters']['cat1'] = 0;
 	core.catalog['filters']['cat2'] = 0;
 	core.catalog['filters']['priceType'] = 0;
 	core.catalog['filters']['cartOnly'] = 0;
 	core.catalog['filters']['cartOnly'] = 0;
+	core.catalog['filters']['dd'] = 0;
 
 
 	core.addHandler('onrequest',core.catalog.closeAllPopups);
@@ -467,9 +468,18 @@ core.catalog.initCatalog=function(cartOnly){
 	if(cartOnly == 1){
 		core.catalog.setFilter('cartOnly',true);
 	}
+	var updateListing = false;
 	if(!isNaN(core.afterCatalogInitCat1Filter)){
 		core.catalog.setFilter('cat1',core.afterCatalogInitCat1Filter);
 		core.afterCatalogInitCat1Filter = null;
+		updateListing = true;
+	}
+	if(ddIdFilter != 0){
+		core.catalog.setFilter('dd',ddIdFilter);
+		updateListing = true;
+	}
+	if(updateListing){
+		core.catalog.updateListing();
 	}
 }
 
