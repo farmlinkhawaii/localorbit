032afc1404d24696a6578554ce04b28ae2394b5d
Tim Bugai <tim@bugai.net>
2014-06-06 13:02:02 -0400

    Create a featured promotion

--- spec/support/domino.rb
+++ spec/support/domino.rb
@@ -297,6 +297,15 @@ module Dom
       end
     end
 
+    class FeaturedPromotionRow < Domino
+      selector ".promotion-row"
+
+      attribute :date
+      attribute :name
+      attribute :title
+      attribute :market
+    end
+
     class MarketRow < Domino
       selector ".market-org-list tbody tr"
 
