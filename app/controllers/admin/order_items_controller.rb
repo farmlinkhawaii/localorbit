2b067e22f2f2f99a19c7180ec0c4a6dc5cc2e18e
Jonathan Pichot <jonathan@pichot.us>
2014-06-27 16:46:00 -0400

    Adds date QueryDefaults to Sold Items
    Conflicts:
    	app/controllers/admin/order_items_controller.rb

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -7,7 +7,9 @@ module Admin
       prepare_filter_data(@order_items)
 
       # initialize ransack and search
-      search = params[:q] || {}
+      @sticky_params = sticky_parameters(request.query_parameters)
+      search = Search::QueryDefaults.new(@sticky_params[:q], :created_at).query
+
       @q = @order_items.search(search)
       @q.sorts = ["order_placed_at desc", "name"] if @q.sorts.empty?
       @order_items = @q.result
